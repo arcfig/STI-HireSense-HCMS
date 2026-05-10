f6f8e6bc56d3495ce39e7e5542c5fa920decbe6f
arcfig <arcfig2@gmail.com>
2026-05-10 14:20:23 +0800

    Force Update

--- hcms-backend/server.js
+++ hcms-backend/server.js
@@ -23,7 +23,8 @@ const authLimiter = rateLimit({
 });
 
 // Middleware to handle JSON and Cross-Origin requests
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cors({
     origin: '*', // Or explicitly define the frontend URL: 'https://purple-barracuda-906917.hostingersite.com'
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
