45a8cfa69b8b2df9aff122e8e58ee9fe731abbf4
arcfig <arcfig2@gmail.com>
2026-05-10 15:37:14 +0800

    LAST change probably

--- hcms-frontend/src/pages/CandidateMatcher.jsx
+++ hcms-frontend/src/pages/CandidateMatcher.jsx
@@ -16,7 +16,7 @@ function CandidateMatcher() {
 
     try {
       // 1. Retrieve the raw data from storage
-      const rawStorageValue = localStorage.getItem('token'); 
+      const rawStorageValue = localStorage.getItem('user'); 
       let userToken = '';
 
       console.log("DIAGNOSTIC 1: Raw Storage Value ->", rawStorageValue);
